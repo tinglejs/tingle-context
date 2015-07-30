0dc3403e5e5ee7c54c5f1fd440007df2f28ec69c
拂山 <jiasong.js@alibaba-inc.com>
2015-07-30 10:36:43 +0800

    merge rem and tap-effect branch

--- demo/ContextDemo.js
+++ demo/ContextDemo.js
@@ -4,5 +4,14 @@
 * @Last Modified by:   caoke
 * @Last Modified time: 2015-07-13 12:38:02
 */
+var Tingle = require('../src/Context');
+console.log(Tingle);
 
-'use strict';
+for (var i = 0; i < 10; i++) {
+    var div = document.createElement('div');
+    div.style.float = 'left';
+    div.style.width = '1rem';
+    div.style.height = '1rem';
+    div.style.background = 'rgba(0, 0, 0, ' + ((i + 1) / 10) + ')';
+    document.body.appendChild(div);
+}
